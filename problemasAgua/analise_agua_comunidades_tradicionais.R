b9d2dd920d90cf02672782c222e529f35400b4bf
Neylson Crepalde <neylsoncrepalde@gmail.com>
2017-06-06 14:09:37 -0300

    Update analise_agua_comunidades_tradicionais.R

--- problemasAgua/analise_agua_comunidades_tradicionais.R
+++ problemasAgua/analise_agua_comunidades_tradicionais.R
@@ -21,8 +21,8 @@ dic <- fread('dicionariodomicilio.csv')
 View(dic)
 ##########################################
 
-com_tradicionais[com_tradicionais == 0] = NA
-freq(com_tradicionais)
+#com_tradicionais[com_tradicionais == 0] = NA
+#freq(com_tradicionais)
 
 freq( CADUNICO$cod_agua_canalizada_fam[CADUNICO$cod_indigena_reside_fam == 1] ) 
 freq( CADUNICO$cod_agua_canalizada_fam[CADUNICO$ind_familia_quilombola_fam == 1] )
@@ -32,7 +32,16 @@ freq( CADUNICO$cod_abaste_agua_domic_fam[CADUNICO$cod_indigena_reside_fam == 1] 
 freq( CADUNICO$cod_abaste_agua_domic_fam[CADUNICO$ind_familia_quilombola_fam == 1] )
 table( CADUNICO$cod_abaste_agua_domic_fam, CADUNICO$ind_parc_mds_fam )
 
-################################################
+##########################################################################
 #Verificar concentração de comunidades tracionais sem água por município
+
+#Separando os banco de dados por comunidades tradicionais
+comunidades_tradicionais = CADUNICO[CADUNICO$cod_indigena_reside_fam == 1 |
+                                      CADUNICO$ind_familia_quilombola_fam == 1 |
+                                      CADUNICO$ind_parc_mds_fam != 0, ]
+
+#Verificando a concentração espacial de domicílios de comunidades tradicionais
+#sem água
+freq(comunidades_tradicionais$cd_ibge[comunidades_tradicionais$cod_agua_canalizada_fam == 2])
 
 
