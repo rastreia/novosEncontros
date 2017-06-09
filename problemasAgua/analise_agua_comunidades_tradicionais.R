1e108ef98d1a7223ddaf5cf4699a4b3fe179d9f3
Rastreia - Avaliação e Monitoramento <rastreiamg@gmail.com>
2017-06-09 09:14:54 -0300

    add totais de atendidos por grupo fam tradicional

--- problemasAgua/analise_agua_comunidades_tradicionais.R
+++ problemasAgua/analise_agua_comunidades_tradicionais.R
@@ -133,3 +133,39 @@ sum(separado_ind_per$count[separado_ind_per$nom_povo_indigena_fam != "" &
 
 sum(separado_qui_per$count[separado_qui_per$cod_agua_canalizada_fam == "Não"])
 
+#Demandas posteriores ---------------
+#######################
+# Subset Vale do Rio Doce - KITS SEAPA
+
+comunidades_vale = comunidades_tradicionais[comunidades_tradicionais$nome_regiao == "Vale do Rio Doce",]
+freq(comunidades_vale$cod_indigena_reside_fam)
+freq(comunidades_vale$ind_familia_quilombola_fam)
+
+#Separando as comunidades tradicionais que interessam, a saber:
+# 203 - família de terreiro - NAO TEM!
+# 205 - família de agricultores familiares
+# 301 - Família assentada da reforma agraria
+# 303 - Família acampada
+# 
+comunidades_vale$ind_parc_mds_fam[comunidades_vale$ind_parc_mds_fam == 0] = NA
+comunidades_vale$ind_parc_mds_fam[comunidades_vale$ind_parc_mds_fam != 203 & 
+                                    comunidades_vale$ind_parc_mds_fam != 205 &
+                                    comunidades_vale$ind_parc_mds_fam != 301 &
+                                    comunidades_vale$ind_parc_mds_fam != 303] = NA
+
+freq(comunidades_vale$ind_parc_mds_fam)
+
+#Total de atendidos
+atendidos = sum(comunidades_vale$qtd_pessoas_domic_fam, na.rm = T); print(atendidos)
+
+#Total de atendidos por nome de comunidade indígena
+comunidades_vale %>% group_by(nom_povo_indigena_fam) %>%
+  summarize(total = sum(qtd_pessoas_domic_fam))
+
+#Total de atendidos por comunidade quilombola
+comunidades_vale %>% group_by(nom_comunidade_quilombola_fam) %>%
+  summarize(total = sum(qtd_pessoas_domic_fam))
+
+#Total de atendidos por famílias tradicionais
+comunidades_vale %>% group_by(ind_parc_mds_fam) %>%
+  summarize(total = sum(qtd_pessoas_domic_fam))
