de49c8ac49385a1c1cef040437f36969ad13565f
Rastreia - Avaliação e Monitoramento <rastreiamg@gmail.com>
2017-06-22 16:46:55 -0300

    Create sedinor_diagnostico_priscila_analises.R

---
+++ problemasAgua/sedinor_diagnostico_priscila_analises.R
@@ -0,0 +1,10 @@
+#####################################
+# Problemas  - ações com água
+# RASTREIA
+# Neylson Crepalde
+#####################################
+
+setwd('~/RASTREIA/NOVOS ENCONTROS/problemas_agua')
+source("sedinor_diagnostico_priscila_le_dados.R")
+#################################################
+
