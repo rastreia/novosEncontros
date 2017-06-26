264431f8159f532425829851f20f089ffe92bc9f
Rastreia - Avaliação e Monitoramento <rastreiamg@gmail.com>
2017-06-26 14:51:23 -0300

    major update

--- problemasAgua/sedinor_diagnostico_priscila_analises.R
+++ problemasAgua/sedinor_diagnostico_priscila_analises.R
@@ -8,3 +8,21 @@ setwd('~/RASTREIA/NOVOS ENCONTROS/problemas_agua')
 source("sedinor_diagnostico_priscila_le_dados.R")
 #################################################
 
+# Comparando categorias semelhantes nas duas variáveis
+table(as.numeric(sedinor_atual[,28]),as.numeric(sedinor_atual[,35]))
+chisq.test(table(as.numeric(sedinor_atual[,28]),as.numeric(sedinor_atual[,35])))
+
+table(as.numeric(sedinor_atual[,31]),as.numeric(sedinor_atual[,38]))
+chisq.test(table(as.numeric(sedinor_atual[,31]),as.numeric(sedinor_atual[,38])))
+
+
+# Renomeando variáveis
+names(sedinor_atual)[c(30,31,38)] = c("Tubulação e reservatório entregue",
+                                      "Tubulação e reservatório não entregue",
+                                      "Tubulação não entregue")
+
+sedinor_atual %>% filter(Equipado == 1) %>% count(`Tubulação não entregue`)
+
+
+# Poços com tubulação e sem energia
+sedinor_atual %>% filter(`Tubulação e reservatório entregue`==1) %>% count(Energizado)
